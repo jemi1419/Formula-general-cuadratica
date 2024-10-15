24475f8b9b75f864009cc0a3815b6ead40dd9a7d
jemi1419 <camj080124hgtlrna2@cbtis217.edu.mx>
2024-10-15 08:15:50 -0600

    Cuadratica 2.0

--- prct.cpp.cpp
+++ prct.cpp.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main() {
 	double a, b, c; 
+	// leer datos
 	cin >> a >> b >> c;
+	// x1
 	cout << "x1: " << (-b+ sqrt(b*b - 4*a*c)) / (2*a) << "\n";
+	// x2
 	cout << "x2: " << (-b-sqrt(b*b-4*a*c))/(2*a) << "\n";
 	return 0;
 }
